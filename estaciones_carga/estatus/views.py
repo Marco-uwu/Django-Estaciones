7fed64ce834ceed83ea51b8b928952b044f2da9a
Marco-uwu <mmaldonadof1900@alumno.ipn.mx>
2024-11-23 16:57:02 -0600

    Modificada vista estatus

--- estaciones_carga/estatus/views.py
+++ estaciones_carga/estatus/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Estaciones, ParametrosMedicion
+from django.contrib import messages
 
 def estatus(request):
-    estaciones = Estaciones.objects.all().values()
     template = loader.get_template('index_estatus.html')
+    estaciones = Estaciones.objects.all().values()
+    if request.method == "POST":
+        try:
+            resultado = "Ok"
+            messages.success(request, resultado)
+        except Exception as e:
+            resultado = "Error"
+            messages.error(request, resultado)
     context = {
         'estaciones' : estaciones,
     }
