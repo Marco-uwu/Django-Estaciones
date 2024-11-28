109cbf08e2334a2249ffa0f398095c51020a8e03
Marco-uwu <mmaldonadof1900@alumno.ipn.mx>
2024-11-27 23:41:45 -0600

    Modificaciones a las urls y autenticación de usuarios.

--- estaciones_carga/inicio/urls.py
+++ estaciones_carga/inicio/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
-    path('', views.inicio, name='inicio'),
     path('inicio/', views.inicio, name='inicio'),
 ]
