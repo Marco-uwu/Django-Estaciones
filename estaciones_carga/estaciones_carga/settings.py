ade2ba0ecc1ebd0511f4750bdf174ad79d619e97
Marco-uwu <mmaldonadof1900@alumno.ipn.mx>
2024-12-07 16:04:14 -0600

    Creación de certificados autofirmados en el servidor para poder establecer conexiones por HTTPS

--- estaciones_carga/estaciones_carga/settings.py
+++ estaciones_carga/estaciones_carga/settings.py
@@ -32,6 +32,7 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
