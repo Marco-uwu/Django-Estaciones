08ed509be7e69e5a128d920538326c50a5f8c040
Marco-uwu <mmaldonadof1900@alumno.ipn.mx>
2024-12-15 03:42:18 -0600

    Agregada interfaz para agregar nuevas estaciones.

--- estaciones_carga/estatus/views.py
+++ estaciones_carga/estatus/views.py
@@ -8,6 +8,22 @@ import warnings
 import paho.mqtt.client as mqtt
 from .decorators import admin_required
 from .claseEstacion import Estacion
+import base64
+import re
+from django.db import transaction, IntegrityError
+
+def procesar_mac(mac_str):
+    # Validar que la dirección MAC esté en el formato "00:11:22:33:44:55"
+    if not re.match(r'^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$', mac_str):
+        return None
+
+    # Convertir la dirección MAC al formato continuo sin los ":" y en mayúsculas
+    dir_mac = mac_str.replace(":", "").upper()
+
+    # Codificar la dirección MAC en Base64
+    mac_bytes = dir_mac.encode('utf-8')
+    mac_base64 = base64.urlsafe_b64encode(mac_bytes).decode('utf-8')
+    return mac_base64
 
 def enviar_apagado_manual(direccion, nombre):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -61,6 +77,31 @@ def estatus(request):
                 page_number = int(pagina)
             else:
                 page_number = 1
+        elif tipo_formulario == "3":
+            nuevo_nombre = request.POST.get('nombre_nueva_estacion')
+            nueva_dir = procesar_mac(request.POST.get('dir_nueva_estacion'))
+
+            if nueva_dir is not None:
+                try:
+                    with transaction.atomic():
+                        if not Estaciones.objects.filter(dir_mac=nueva_dir).exists():
+                            id_regla_instancia = ReglasMedicion.objects.get(pk=1)
+                            id_tarifa_instancia = Tarifas.objects.get(pk=1)
+                            nueva_estacion = Estaciones(
+                                id_regla=id_regla_instancia,
+                                nombre=nuevo_nombre,  # Asegúrate de que 'nuevo_nombre' esté definido
+                                estado='Fuera de servicio',
+                                dir_mac=nueva_dir,
+                                id_tarifa=id_tarifa_instancia
+                            )
+                            nueva_estacion.save()
+                            resultado = "Estación insertada exitosamente."
+                        else:
+                            resultado = "Ya existe una estación con la dirección MAC especificada."
+                except IntegrityError:
+                    resultado = "Ocurrió un error al intentar insertar la estación."
+            else:
+                resultado = "Error: La dirección MAC no está en el formato solicitado. Use 00:11:22:33:44:55."
         else:
             resultado = "Error en solicitud POST"
     
