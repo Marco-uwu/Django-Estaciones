2c0c189f5d1b13ef11c5f036702dcca846fe8077
Marco-uwu <mmaldonadof1900@alumno.ipn.mx>
2024-12-15 01:19:44 -0600

    Modificaciones a la interfaz "Reportes", agregada paginación y botones.

--- estaciones_carga/reportes/views.py
+++ estaciones_carga/reportes/views.py
@@ -8,9 +8,31 @@ from .decorators import admin_required
 
 @admin_required
 def reportes(request):
-    template = loader.get_template('index_reportes.html')
-    reportes = Reportes.objects.all().order_by('id')[:5]
+
+    reportes = Reportes.objects.all().order_by('id')
+    estaciones = Estaciones.objects.all()
+
+    page_number = 1
+    
+    if request.method == "POST":
+        tipo_formulario = request.POST.get('id_formulario')
+        if tipo_formulario == "1":
+            id_filtro_estacion = request.POST.get('estacion')
+            page_number = int(request.POST.get('pagina', '1'))
+
+            if id_filtro_estacion:
+                reportes = Reportes.objects.filter(id_medicion__id_estacion=id_filtro_estacion).order_by('id')
+        else:
+            mensaje = "Error en la solicitud POST"
+
+    paginator = Paginator(reportes, 15)
+    page_obj = paginator.get_page(page_number)
+
     context = {
         'reportes' : reportes,
+        'estaciones' : estaciones,
+        'pagina_reportes' : page_obj, 
+        'mensaje' : page_obj,
     }
+    template = loader.get_template('index_reportes.html')
     return HttpResponse(template.render(context, request))
